65b4ce57619b79bd26fb04ca5193eac7e5be5e1f
Jonathan Jesús Herrera López <jonathan_herrera117@hotmail.com>
2022-08-26 13:56:10 -0500

    Format fixes

--- Screens/Home.js
+++ Screens/Home.js
@@ -10,49 +10,43 @@ const subirClave = async (id) => {
   await AsyncStorage.setItem("id", id);
 }
 
-const bajarClave = async ()  =>{
-    // return jsonValue != null ? JSON.parse(jsonValue):null;
-    // const jsonValue = await AsyncStorage.getItem("id");
-    return await AsyncStorage.getItem("id");
-} 
-//await AsyncStorage.getItem("id");
-
+const bajarClave = async () => await AsyncStorage.getItem("id");
 
 const Home = ({route}) =>{
   const [dataGlobal, setdataGlobal] = useState();
-  const [clave, setClave] = useState("");
 
   useEffect(() => {
-    let scaneo = route.params.datos;
+    let escaneo = route.params.datos;
     subirClave(escaneo);
     GenerarQR();
   }, []);
 
   const getClave = async () => await bajarClave();
   
-  // CLAVE TOMADA DEL SISTEMA 
-  var escaneo = route.params.datos;
-  subirClave(escaneo);
-  
   //FUNCION PARA GENERAR QRs
   const GenerarQR = async () =>{
-      var fecha = new Date().toDateString();
-      fecha = fecha.split(" ")
+    let fecha = new Date().toDateString().split(" ");
       
-    var id = await getClave();
-    var hora = new Date().getHours();
-    var min = new Date().getMinutes();
-    var sec = new Date().getSeconds();
+    let id = await getClave();
+    let hora = new Date().getHours();
+    let min = new Date().getMinutes();
+    let sec = new Date().getSeconds();
 
-    var data = '{"id": "' + id + '", "anio":' + fecha[3] +', "mes":"' + fecha[1] + '", "diaNum":' + fecha[2] + ', "dia":"' + fecha[0] + '"';
-    data += ', "hora":' + hora + ', "min":' + min + ', "sec": ' + sec + '}'
-    console.log("DATOS: " +data);
+    let data = `{
+      "id": ${id}, 
+      "anio": ${fecha[3]}, 
+      "mes": ${fecha[1]}, 
+      "diaNum": ${fecha[2]}, 
+      "dia": ${fecha[0]}, 
+      hora": ${hora}, 
+      "min": ${min}, 
+      "sec": ${sec}
+    }`;
     setdataGlobal(data);
-    //setdataGlobal(escaneo);
   }
   return (
     <View style={styles.container}>
-        <Text>Info: {route.params.datos}</Text>
+      <Text>Info: {route.params.datos}</Text>
       <QRCode value={dataGlobal} size={300} logo={logoINDEREQ}/>      
       <Button onPress={GenerarQR} title="Generar QR"/>
       <StatusBar style="auto" />
