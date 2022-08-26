e628a43bbecbec2c4e191ca4be53bb19c7f7d97c
Jorge Bernal <jorgealejandrobernal@gmail.com>
2022-08-26 10:29:34 -0500

    Modified App.js
    testing with asyncronous information

--- App.js
+++ App.js
@@ -1,65 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import React, {useState} from 'react';
-// import QRDinamico from './components/QRDinamico';
-// import logo from '.INDEREQ/images';
-
+import React, { useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import QRCode from 'react-native-qrcode-svg';
 
+let logoINDEREQ = require('./images/logo.png');
+
+const subirClave = async (id) => {
+  await AsyncStorage.setItem("id", id);
+}
+
+const bajarClave = async ()  => {
+  const res = await AsyncStorage.getItem("id");
+}
 
 export default function App() {
-  const BotonEspecial = () =>{
+  const [dataGlobal, setdataGlobal] = useState();
+  
+  // CLAVE TOMADA DEL SISTEMA 
+  
+  subirClave("hola");
+  var id = "IdPrueba";
+  bajarClave();
+
+  //FUNCION PARA GENERAR QRs
+  const GenerarQR = () =>{
     var fecha = new Date().toDateString();
     fecha = fecha.split(" ")
-    // console.log(fecha);
     
-    var id = "MarioNinja1526";
     var hora = new Date().getHours();
     var min = new Date().getMinutes();
     var sec = new Date().getSeconds();
-    var pinAleatorio = [];
-    // var pinAleatorio = Math.round(Math.random()*10);
-    for (let i = 0; i < 4; i++) {
-      var pinAleProv;
-      pinAleProv = Math.round(Math.random()*10)
-      if (pinAleProv <= 9) {
-        pinAleatorio.push(pinAleProv)   
-      } else {
-        i--;
-      }
-    }
 
-    var pinFinal = '' + pinAleatorio[0] + pinAleatorio[1] + pinAleatorio[2] + pinAleatorio[3]; 
-    // console.log("PIN ALEATORIO[0]" + pinAleatorio[0]);
-    // console.log("PIN ALEATORIO[1]" + pinAleatorio[1]);
-    // console.log("PIN ALEATORIO[2]" + pinAleatorio[2]);
-    // console.log("PIN ALEATORIO[3]" + pinAleatorio[3]);
-    // console.log("PIN ALEATORIO FULL" + pinAleatorio);
-    var data = '{"anio":' + fecha[3] +', "mes":"' + fecha[1] + '", "diaNum":' + fecha[2] + ', "dia":"' + fecha[0] + '"';
-    data += ', "hora":' + hora + ', "min":' + min + ', "sec": ' + sec + ', "pin":' + pinFinal + '}'
-    // var PRUEBA1 = {"anio" : fecha[3]};
-    console.log("DATA>" + data);
+    var data = '{"id": "' + id + ', "anio":' + fecha[3] +', "mes":"' + fecha[1] + '", "diaNum":' + fecha[2] + ', "dia":"' + fecha[0] + '"';
+    data += ', "hora":' + hora + ', "min":' + min + ', "sec": ' + sec + '}'
+    console.log(data);
     setdataGlobal(data);
   }
-  let logoINDEREQ = require('./images/logo.png');
-  const [dataGlobal, setdataGlobal] = useState();
   return (
     <View style={styles.container}>
-      {/* <Text>{data}</Text> */}
-      <QRCode value={dataGlobal} size={300} logo={logoINDEREQ}/>
-      {/* <QRDinamico/> */}
-      
-      <Button onPress={BotonEspecial} title="holamundo">Hola Presioname</Button>
+      <QRCode value={dataGlobal} size={300} logo={logoINDEREQ}/>      
+      <Button onPress={GenerarQR} title="Generar QR"/>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ABC',
     alignItems: 'center',
     justifyContent: 'center',
   },
